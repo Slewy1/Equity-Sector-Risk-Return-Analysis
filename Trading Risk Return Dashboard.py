0ed9a9d499e796ea10cac1ef4b5386668c003581
Joshua Chung <124689825+Slewy1@users.noreply.github.com>
2025-11-25 16:32:07 +0000

    Added monthly return calculations and date extraction logic
    Kept in some debugging code for future. Code prints out monthly returns and monthly dates for the XLE.csv

--- Trading Risk Return Dashboard.py
+++ Trading Risk Return Dashboard.py
@@ -20,9 +20,36 @@ with open(r"E:\Python stuff\Project1\XLE.csv", newline="", encoding="utf-8-sig")
         prices.append(float(row['Price']))
 
 
-print(dates[:5])
-print(prices[:5])
+# print(dates[:5])
+# print(prices[:5])
 
-excelDate = "11/21/2025"
-correctedDate = datetime.strptime(excelDate, "%m/%d/%Y")
-print(correctedDate)
+# excelDate = "11/21/2025"
+# correctedDate = datetime.strptime(excelDate, "%m/%d/%Y")
+# print(correctedDate)
+
+dates.reverse()
+prices.reverse()
+
+monthly_returns = []
+monthly_dates = []
+
+current_month = (dates[0].year, dates[0].month)
+first_price_of_month = prices[0]
+
+
+for i in range(1, len(dates)):
+    this_month = (dates[i].year, dates[i].month)
+
+    if this_month == current_month:
+        continue
+    else:
+        monthly_return = (
+            prices[i-1] - first_price_of_month)/first_price_of_month
+        monthly_returns.append(monthly_return)
+        monthly_dates.append(dates[i-1])
+        current_month = this_month
+        first_price_of_month = prices[i]
+
+
+print(monthly_returns)
+print(monthly_dates)
