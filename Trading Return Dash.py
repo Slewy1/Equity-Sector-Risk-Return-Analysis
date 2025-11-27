8572fdc5131fbcde6c46c7206f255383a91e5377
Joshua Chung <124689825+Slewy1@users.noreply.github.com>
2025-11-27 18:15:04 +0000

    Updated Trading Dash
    Issue with BTC dataset due to high values including commas which the ReturnsFunc.py code cannot handle yet

--- Trading Return Dash.py
+++ Trading Return Dash.py
@@ -1,3 +1,29 @@
-import ReturnsFunc
+import ReturnsFunc as rf
+import matplotlib.pyplot as plt
 
-avg, vol = ReturnsFunc.dashboard("XLE.csv")
+avgE, volE = rf.dashboard("XLE.csv")
+avgF, volF = rf.dashboard("XLF.csv")
+avgK, volK = rf.dashboard("XLK.csv")
+avgP, volP = rf.dashboard("XLP.csv")
+avgV, volV = rf.dashboard("XLV.csv")
+# avgBTC, volBTC = rf.dashboard("BTCUSD.csv")
+avgGd, volGd = rf.dashboard("GLD.csv")
+
+results = {
+    "XLE": (avgE, volE),
+    "XLV": (avgV, volV),
+    "XLF": (avgF, volF),
+    # "BTC": (avgBTC, volBTC), 
+    "GLD": (avgGd, volGd)
+}
+
+print(results)
+
+print(avgE, volE)
+print(avgV, volV)
+print(avgF, volF)
+
+
+plt.plot()
+plt.show
+
