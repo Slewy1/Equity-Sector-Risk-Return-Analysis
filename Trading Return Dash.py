e0beed7db6676e0601a91a3f4687ca2efa754d1d
Joshua Chung <124689825+Slewy1@users.noreply.github.com>
2026-02-25 18:27:07 +0000

    Update Trading Return Dash.py
    Updated scatterplot, dark mode + quadrant WIP. Temporarily removed BTCUSD from the code.

--- Trading Return Dash.py
+++ Trading Return Dash.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# data = ["XLE.csv", "XLF.csv", "XLK.csv", "XLP.csv", "XLV.csv", "GLD.csv", "BTCUSD.csv"]
 data = ["XLE.csv", "XLF.csv", "XLK.csv",
-        "XLP.csv", "XLV.csv", "GLD.csv", "BTCUSD.csv"]
+        "XLP.csv", "XLV.csv", "GLD.csv"]
 results = {}
 
 for d in data:
@@ -42,15 +43,29 @@ x = np.array(list(ranking_avg.values()))
 y = np.array(list(ranking_vol.values()))
 label = list(results.keys())
 
+plt.style.use('dark_background')
+plt.figure(figsize=(16, 9))
 for i in range(len(label)):
-    plt.scatter(x[i] * 100, y[i] * 100)
-    plt.text(x[i] * 100 + 0.05, y[i] * 100 - 0.15, label[i], fontweight="bold")
+    plt.scatter(x[i] * 100, y[i] * 100, s=80, edgecolor='w')
+    plt.text(x[i] * 100 + 0.015, y[i] * 100 -
+             0.01, label[i], fontweight="bold", fontsize=11)
+plt.gca().invert_yaxis()
+plt.xlim(0, 2)
+plt.ylim(3, 9)
 
+limits_x = plt.xlim(0, 2)
+limits_y = plt.ylim(3, 9)
+
+mid_x = (limits_x[1] - limits_x[0])/2
+mid_y = (limits_y[1] - limits_y[0])/2
+
+plt.axvline(x=1, color='r', label='axvline - full height',
+            linewidth=4)
+plt.axhline(y=6, color='r', label='axvline - full height',
+            linewidth=4)
 
 plt.grid(True)
 plt.xlabel("Average Monthly Return %")
 plt.ylabel("Average Monthly Volatility %")
 plt.title("Trading Risk Return Scatterplot")
 plt.show()
-
-
